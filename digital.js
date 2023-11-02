3f88948cdf6ca8f6e7acc2c7b17579e097331024
Anjusha-priyadarsan <bjanjusha@gmail.com>
2023-11-02 21:57:12 +0530

    Digital Clock

--- digital.js
+++ digital.js
@@ -4,8 +4,21 @@ function clockTime()
     let hour=date.getHours()
     let minute=date.getMinutes()
     let second=date.getSeconds()
+    let session = "AM";
 
-    clock.innerHTML=`${hour}:${minute}:${second}`
+    if(hour == 0){
+        hour = 12;
+    }
+    if(hour > 12){
+        hour = hour - 12;
+        session = "PM";
+     }
+  
+     hour = (hour < 10) ? "0" + hour : hour;
+     minute = (minute < 10) ? "0" + minute : minute;
+     second = (second < 10) ? "0" + second : second;
+
+    clock.innerHTML=`${hour}:${minute}:${second}  ${session}`
 
     setTimeout(()=>{clockTime(),1000})
 }
